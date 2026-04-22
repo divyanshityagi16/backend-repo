0168d54c6c13a11a95b3d634644ac83f6d81708e
divyanshityagi16 <divyanshi.2428cseai2@kiet.edu>
2026-04-22 17:11:33 +0530

    removed node_modules

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authMiddleware = require("./middleware/authMiddleware");
+require("dotenv").config();
+
+const app = express();
+
+// ✅ THIS MUST BE HERE (BEFORE ROUTES)
+app.use(express.json());
+app.use(cors());
+
+// DB connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+// ✅ ROUTES AFTER middleware
+app.use("/api/auth", require("./routes/auth"));
+
+
+app.get("/api/protected", authMiddleware, (req, res) => {
+  res.json({
+    msg: "Access granted",
+    user: req.user
+  });
+});
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log("Server running"));
